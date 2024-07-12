17d17db7cd29c6d263bb750d131be54b45be4f1f
Bram Haenraets <83586694+Bram-Viqal@users.noreply.github.com>
2024-07-12 13:12:32 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,30 @@
-const CACHE_NAME = 'viqal-cache-v1';
-const urlsToCache = [
-    '/',
-    '/index.html',
-    'https://cdn.prod.website-files.com/654902ad9c3f74c1bb3812ae/js/webflow.c9d2bd03f.js',
-    // Add other URLs to cache as needed
-];
+module.exports = (req, res) => {
+  res.setHeader('Content-Type', 'application/javascript');
+  res.send(`
+    const CACHE_NAME = 'viqal-cache-v1';
+    const urlsToCache = [
+      '/',
+      '/index.html',
+      'https://cdn.prod.website-files.com/654902ad9c3f74c1bb3812ae/js/webflow.c9d2bd03f.js',
+      // Add other URLs to cache as needed
+    ];
 
-self.addEventListener('install', event => {
-    event.waitUntil(
+    self.addEventListener('install', event => {
+      event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(urlsToCache);
-            })
-    );
-});
+          .then(cache => {
+            return cache.addAll(urlsToCache);
+          })
+      );
+    });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
+    self.addEventListener('fetch', event => {
+      event.respondWith(
         caches.match(event.request)
-            .then(response => {
-                return response || fetch(event.request);
-            })
-    );
-});
+          .then(response => {
+            return response || fetch(event.request);
+          })
+      );
+    });
+  `);
+};
